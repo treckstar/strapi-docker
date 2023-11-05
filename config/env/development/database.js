e2011148e82bfaa74c04cf0486acc5277ee5a171
Brandon Trecki <treckstar@gmail.com>
2023-11-05 03:42:22 -0500

    fix: update swagger url & db envs
    Regenerate docs

---
+++ config/env/development/database.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = 'sqlite';
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
--- config/plugins.js
+++ config/plugins.js
@@ -24,16 +24,50 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         delete: {},
       },
+    },
+  },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "2.0.0",
+        title: "DOCUMENTATION",
+        description: "",
+        termsOfService: "https://treckstar.net/terms",
+        contact: {
+          name: "Brandon Trecki",
+          email: "treckstar@twitter.com",
+          url: "https://treckstar.net",
+        },
+        license: {
+          name: "Apache 2.0",
+          url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+      },
+      "x-strapi-config": {
+        // Leave empty to ignore plugins during generation
+        plugins: ["upload", "users-permissions"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: env("BASE_URL") + `/api`, description: "Production server" },
+      ],
+      externalDocs: {
+        description: "Find out more",
+        url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
+      },
+      security: [{ bearerAuth: [] }],
     },
   },
 });
