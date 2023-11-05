8e013f26c77b5474bb4ca1de0eb5efebbb3dd18f
Brandon Trecki <treckstar@gmail.com>
2023-11-05 12:44:57 -0500

    fix: add documentation settings.

--- config/plugins.js
+++ config/plugins.js
@@ -41,7 +41,7 @@ module.exports = ({ env }) => ({
     config: {
       openapi: "3.0.0",
       info: {
-        version: "2.0.0",
+        version: "2.2.4",
         title: "DOCUMENTATION",
         description: "",
         termsOfService: "https://treckstar.net/terms",
